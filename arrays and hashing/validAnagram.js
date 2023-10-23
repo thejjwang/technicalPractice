37ad3c94f31fa0955ece4ccef4cf7f0ea2b99ad7
thejjwang <jonathanjacobwang@gmail.com>
2023-10-23 11:25:03 -0700

    validAnagram psuedo

---
+++ arrays and hashing/validAnagram.js
@@ -0,0 +1,26 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+// function taking in 2 strings
+// if string1.length !== string2.length can automatically return false
+// for loop through string1 
+// check if string1[i] != string2[i] return false
+// otherwise return true
