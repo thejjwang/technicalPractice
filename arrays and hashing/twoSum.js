dc8759a716828d52950fc707f1b466c22d513c89
thejjwang <jonathanjacobwang@gmail.com>
2023-10-25 11:19:56 -0700

    twoSum

--- arrays and hashing/twoSum.js
+++ arrays and hashing/twoSum.js
@@ -1,10 +1,7 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 // You can return the answer in any order.
 
- 
 
 // Example 1:
 
@@ -27,3 +24,20 @@
 // -109 <= nums[i] <= 109
 // -109 <= target <= 109
 // Only one valid answer exists.
+
+// function taking in nums array and a target number
+// loop through nums array
+// loop through nums array backwards
+// if nums[i] + nums[j] === target return true
+
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+console.log(twoSum([2,7,11,15], 9))
