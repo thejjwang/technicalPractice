41054f7c7759112e48c5fb60c614e45e7463446b
thejjwang <jonathanjacobwang@gmail.com>
2023-10-30 14:11:53 -0700

    secondGreatestNum

---
+++ random practice/secondGreatestNum.js
@@ -0,0 +1,21 @@
+const findSecondGreatestNum = (nums) => {
+    let greatestNum = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > greatestNum) {
+            greatestNum = nums[i];
+        }
+    }
+    let greatestNumIndex = nums.indexOf(greatestNum);
+    delete nums[greatestNumIndex];
+
+    let newGreatestNum = nums[0];
+
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] > newGreatestNum) {
+            newGreatestNum = nums[j];
+        }
+    }
+    return newGreatestNum;
+}
+
+console.log(findSecondGreatestNum([-12, -1, 0, -100 ]));
