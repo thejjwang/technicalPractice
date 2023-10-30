c87e194532b690614f9420b3508efa0cffe49048
thejjwang <jonathanjacobwang@gmail.com>
2023-10-30 14:16:47 -0700

    refactored secondGreatest

--- random practice/secondGreatestNum.js
+++ random practice/secondGreatestNum.js
@@ -1,21 +1,28 @@
 const findSecondGreatestNum = (nums) => {
-    let greatestNum = nums[0];
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] > greatestNum) {
-            greatestNum = nums[i];
-        }
-    }
-    let greatestNumIndex = nums.indexOf(greatestNum);
-    delete nums[greatestNumIndex];
+    let greatestNum = Math.max(...nums);
+    console.log(greatestNum);
 
-    let newGreatestNum = nums[0];
+    let indexGreatestNum = nums.indexOf(greatestNum);
+    nums.splice(indexGreatestNum, 1);
 
-    for (let j = 0; j < nums.length; j++) {
-        if (nums[j] > newGreatestNum) {
-            newGreatestNum = nums[j];
-        }
-    }
-    return newGreatestNum;
+    return Math.max(...nums)
+    // let greatestNum = nums[0];
+    // for (let i = 1; i < nums.length; i++) {
+    //     if (nums[i] > greatestNum) {
+    //         greatestNum = nums[i];
+    //     }
+    // }
+    // let greatestNumIndex = nums.indexOf(greatestNum);
+    // delete nums[greatestNumIndex];
+
+    // let newGreatestNum = nums[0];
+
+    // for (let j = 0; j < nums.length; j++) {
+    //     if (nums[j] > newGreatestNum) {
+    //         newGreatestNum = nums[j];
+    //     }
+    // }
+    // return newGreatestNum;
 }
 
 console.log(findSecondGreatestNum([-12, -1, 0, -100 ]));
