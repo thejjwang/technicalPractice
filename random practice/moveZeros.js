f086423083d865c37da1e3d60f52625f55e7a5b2
thejjwang <jonathanjacobwang@gmail.com>
2023-11-15 21:05:55 -0800

    moveZeroes

--- random practice/moveZeros.js
+++ random practice/moveZeros.js
@@ -1,35 +1,19 @@
-// Given an integer array nums, move all 0's to the end of it while maintaining 
-// the relative order of the non-zero elements.
-// Note that you must do this in-place without making a copy of the array.
+// function taking in array of integers 
+// initalize new arr
+// loop through array.lenght
+// if array[i] === 0 then slice that value
+// also array.push(0) for each slice
 
-// Example 1:
-
-// Input: nums = [0,1,0,3,12]
-// Output: [1,3,12,0,0]
-// Example 2:
-
-// Input: nums = [0]
-// Output: [0]
-
-// function taking in nums array 
-// initialize numbers arr
-// initialize zeros arr
-// loop through nums 
-// check if nums[i] === 0 
-// if its 0 push it to zeros arr
-// else push to numbers arr
-// return and concat the arrays
-
-const moveZeros = (arr) => {
-    let numbersArr = [];
-    let zerosArr = [];
-    for (let num of arr) {
-        if (num === 0) {
-            zerosArr.push(num);
+const moveZeroes = (array) => {
+    let zeroArr = [];
+    let numArr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 0) {
+            zeroArr.push(array[i])
         } else {
-            numbersArr.push(num);
+            numArr.push(array[i])
         }
     }
-    return numbersArr.concat(zerosArr);
+    return numArr.concat(zeroArr)
 }
-console.log(moveZeros([0,1,0,3,12]))
+console.log(moveZeroes([0,1,0,3,12]))
