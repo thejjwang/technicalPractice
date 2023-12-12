a5c557b4d9bf360b5cbf9706df07bdb812afcc64
thejjwang <jonathanjacobwang@gmail.com>
2023-12-11 22:44:30 -0800

    remove element

---
+++ arrays/removeElement.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ // function taking in nums and val
+ // nums = []
+ // val = integer 
+ // loop through arr 
+ // for (let i = 0; i < nums.length -1; i++)
+ // remove the integers that the user inputted for val
+ // if nums[i] === val, remove in place
+ // then nums.splice(i)
+ // return nums.length after removing elements
+ var removeElement = function(nums, val) {
+    
+ };
